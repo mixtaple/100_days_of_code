b5e68c48c3b3027d4b6baa2e24cf781721f8dc24
alxSukhov <asukhov.webycorp.com>
2024-01-12 09:47:56 +0300

    Counter App final version

--- 1. Counter/script.js
+++ 1. Counter/script.js
@@ -1,21 +1,28 @@
-let value = 0;
-const buttonPlus = document.querySelector('.button__plus');
-const buttonMinus = document.querySelector('.button__minus');
-const buttonReset = document.querySelector('.button__reset');
+let value = document.querySelector('.counter__value').textContent
+  ? document.querySelector('.counter__value').textContent
+  : 0;
 const counterValue = document.querySelector('.counter__value');
-
+const counterButtons = document.querySelector('.counter__buttons');
 counterValue.textContent = value;
-buttonPlus.addEventListener('click', () => {
-  value += 1;
-  counterValue.textContent = value;
-});
 
-buttonMinus.addEventListener('click', () => {
-  value -= 1;
-  counterValue.textContent = value;
+counterButtons.addEventListener('click', event => {
+  // event.preventDefault();
+  if (event.target.classList.contains('button__plus')) {
+    value += 1;
+  } else if (event.target.classList.contains('button__minus')) {
+    value -= 1;
+  } else if (event.target.classList.contains('button__reset')) {
+    value = 0;
+  }
+  setValue(value);
 });
-
-buttonReset.addEventListener('click', () => {
-  value = 0;
+const setValue = () => {
+  if (value < 0) {
+    counterValue.style.color = 'red';
+  } else if (value > 0) {
+    counterValue.style.color = 'green';
+  } else {
+    counterValue.style.color = 'white';
+  }
   counterValue.textContent = value;
-});
+};
